a9ca81dfa238ae85e6519d05da8cb743e28aa830
Karl Martin <KarlMartin@DESKTOP-ECPCQRP>
2020-07-13 19:59:16 +0300

    Changed response

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = knex({
     }
 });
 
-app.get('/', (req, res) => { res.send(database.users) });
+app.get('/', (req, res) => { res.send('App is working') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
